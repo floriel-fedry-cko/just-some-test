52a2102dcb78c04744dbe4cda0164e15b3e91bc9
floriel-fedry-cko <floriel.fedry@checkout.com>
2018-06-28 10:12:29 +0100

    rename project to CheckoutMerchantAPI

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "just-some-test",
+    name: "CheckoutMerchantAPI",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "just-some-test",
-            targets: ["just-some-test"]),
+            name: "CheckoutMerchantAPI",
+            targets: ["CheckoutMerchantAPI"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,13 +21,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "just-some-test",
+            name: "CheckoutMerchantAPI",
             dependencies: [
                 "Alamofire",
                 "Result",
             ], path: "Sources"),
         .testTarget(
-            name: "just-some-testTests",
-            dependencies: ["just-some-test"]),
+            name: "CheckoutMerchantAPITests",
+            dependencies: ["CheckoutMerchantAPI"]),
     ]
 )
